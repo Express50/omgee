9ca1a75fd9eff7ba67402b322103b3d05f873b95
David Landsman <david.landsman@mail.utoronto.ca>
2017-09-07 17:38:18 -0400

    Add tryCatch for dm method + calculate cov. probability for P1

--- sim/sim.r
+++ sim/sim.r
@@ -4,16 +4,20 @@ library(OverdispersionModelsInR)
 run_simulation <- function(num.sim, p.vec, rho, num.clus, clus.size, ...) {
   ndim <- length(p.vec)
   mord <- ndim - 1
-  # gee.ind.glogit <- vector("list", num.sim)
-  # gee.ind.ident <- vector("list", num.sim)
-  # # gee.exch.glogit <- vector("list", num.sim)
-  # gee.exch.ident <- vector("list", num.sim)
-  dm.est.mat <- matrix(0, num.sim, ndim + 1)
-  dm.se.mat <- matrix(0, num.sim, ndim - 1)
+
+  dm.err <- 0
+  dm.est.mat <- matrix(NA, num.sim, ndim + 1)
+  dm.se.mat <- matrix(NA, num.sim, ndim - 1)
+  dm.ci.mat <- matrix(NA, num.sim, 3)
+
   gee.ind.est <- matrix(0, num.sim, ndim)
   gee.ind.se <- matrix(0, num.sim, ndim - 1)
-  gee.exch.est <- matrix(0, num.sim, ndim + 2)
-  gee.exch.se <- matrix(0, num.sim, ndim - 1)
+  gee.ind.ci <- matrix(0, num.sim, 3)
+
+  gee.exch.err <- 0
+  gee.exch.est <- matrix(NA, num.sim, ndim + 1)
+  gee.exch.se <- matrix(NA, num.sim, ndim - 1)
+  gee.exch.ci <- matrix(NA, num.sim, 3)
 
   for (i in 1:num.sim) {
       if (i %% 100 == 0) cat(sprintf("Running simulation rep %d\n", i))
@@ -26,25 +30,74 @@ run_simulation <- function(num.sim, p.vec, rho, num.clus, clus.size, ...) {
 
       ind <- gmo(dat, link = "identity")
       gee.ind.est[i, ] <- ind$p.vec
-      gee.ind.se[i, ] <- diag(sqrt(ind$var.mat))
-      exch <- gmo(dat, corstr = "exchangeable", link="identity", ...)
-      gee.exch.est[i, ] <- c(exch$p.vec, exch$rho, exch$error)
-      gee.exch.se[i, ] <- diag(sqrt(exch$var.mat))
+      ind.se <- diag(sqrt(ind$var.mat))
+      ind.ci <- get_conf_int(ind$p.vec[1], ind.se[1])
+      gee.ind.se[i, ] <- ind.se
+      gee.ind.ci[i, ] <- c(ind.ci,
+                           ind.ci[1] <= p.vec[1] && p.vec[1] <= ind.ci[2])
 
-      dm.est <- fit.dm.mle(t(dat), rowSums(dat), var.names = c(sprintf("P%d", seq(1:ndim)), "Rho"))
-      dm.est.mat[, ] <- dm.est[[1]][, 1]
-      dm.se.mat[i, ] <- dm.est[[1]][1:(ndim - 1), 2]
+      exch <- gmo(dat, corstr = "exchangeable", link="identity", ...)
+
+      if (exch$error) {
+        gee.exch.err <- gee.exch.err + 1
+      } else {
+        exch.se <- diag(sqrt(exch$var.mat))
+        exch.ci <- get_conf_int(exch$p.vec[1], exch.se[1])
+
+        gee.exch.est[i, ] <- c(exch$p.vec, exch$rho)
+        gee.exch.se[i, ] <- exch.se
+        gee.exch.ci[i, ] <- c(exch.ci,
+                              exch.ci[1] <= p.vec[1] && p.vec[1] <= exch.ci[2])
+      }
+
+      dm.est <- tryCatch({
+        fit.dm.mle(t(dat), rowSums(dat), var.names = c(sprintf("P%d", seq(1:ndim)), "Rho"))
+      },
+      error = function (e) {
+        warning(e)
+        return(NA)
+      })
+
+      if (!is.list(dm.est) && is.na(dm.est)) {
+        dm.err <- dm.err + 1
+      } else {
+        dm.se <- dm.est[[1]][1:(ndim - 1), 2]
+        dm.ci <- get_conf_int(dm.est[[1]][, 1][1], dm.se[1])
+
+        dm.est.mat[i, ] <- dm.est[[1]][, 1]
+        dm.se.mat[i, ] <- dm.se
+        dm.ci.mat[i, ] <- c(dm.ci,
+                            dm.ci[1] <= p.vec[1] && p.vec[1] <= dm.ci[2])
+      }
   }
 
-  # print(gee.exch.est)
+  gee.exch.est <- na.omit(gee.exch.est)
+  gee.exch.se <- na.omit(gee.exch.se)
+  gee.exch.ci <- na.omit(gee.exch.ci)
+
+  dm.est.mat <- na.omit(dm.est.mat)
+  dm.se.mat <- na.omit(dm.se.mat)
+  dm.ci.mat <- na.omit(dm.ci.mat)
 
   gee.ind.empse <- sqrt(colVars(gee.ind.est)[1:mord])
+  gee.exch.empse <- sqrt(colVars(gee.exch.est)[1:mord])
+  dm.empse <- sqrt(colVars(dm.est.mat)[1:mord])
 
   return(list(
     gee.ind.est = gee.ind.est,
     gee.ind.se = gee.ind.se,
     gee.ind.empse = gee.ind.empse,
-    gee.exch.est = gee.exch.est
+    gee.ind.ci = gee.ind.ci,
+    gee.exch.err = gee.exch.err,
+    gee.exch.est = gee.exch.est,
+    gee.exch.se = gee.exch.se,
+    gee.exch.empse = gee.exch.empse,
+    gee.exch.ci = gee.exch.ci,
+    dm.err = dm.err,
+    dm.est = dm.est.mat,
+    dm.se = dm.se.mat,
+    dm.empse = dm.empse,
+    dm.ci = dm.ci.mat
   ))
 }
 
@@ -52,29 +105,28 @@ res <- run_simulation(num.sim = 500,
                       # p.vec = c(0.3, 0.2, 0.4),
                       p.vec = c(0.9, 0.1),
                       rho = 0.7,
-                      num.clus = 150,
+                      num.clus = 100,
                       clus.size = 2)
 
 cat("gee.ind: \n")
 print(colMeans(res$gee.ind.est))
 print(colMeans(res$gee.ind.se))
 print(res$gee.ind.empse)
-
-cat("gee.exch: \n")
-errors = which(res$gee.exch.est[, 4] == 1)
-gee.exch.est.clean <- res$gee.exch.est
-if (length(errors) > 0) gee.exch.est.clean <- res$gee.exch.est[-errors, ]
-print(colMeans(gee.exch.est.clean))
-
-# print(colMeans(gee.ind.est))
-# print(colMeans(gee.ind.se))
-# print(sqrt(colVars(gee.ind.est)))
+print(mean(res$gee.ind.ci[, 3]))
 #
-# print(colMeans(res$dm.est))
-# print(colMeans(res$dm.se))
-# print(sqrt(colVars(res$dm.est)))
-
-
+cat("gee.exch: \n")
+print(colMeans(res$gee.exch.est))
+print(colMeans(res$gee.exch.se))
+print(res$gee.exch.empse)
+print(mean(res$gee.exch.ci[, 3]))
+cat("num errors: ", res$gee.exch.err, "\n")
+#
+cat("dm: \n")
+print(colMeans(res$dm.est))
+print(colMeans(res$dm.se))
+print(res$dm.empse)
+print(mean(res$dm.ci[, 3]))
+cat("num errors: ", res$dm.err, "\n")
 
 # FOR REFERENCE
 # library(msm)
