a7789272cb27813c681f5a8ce481e653065d9002
David Landsman <david.landsman@mail.utoronto.ca>
2018-05-08 18:19:44 -0400

    Add support for binomial BI

--- R/bi.r
+++ R/bi.r
@@ -1,17 +1,21 @@
-bi <- function (gmo, const.vec) {
+bi <- function (gmo, const.vec = NULL) {
   if (!(class(gmo) == "gmo.ident"))
     stop('gmo must be calculated using link=identity')
   m <- gmo$ndim - 1
-  if (!(length(const.vec) == m))
+  if (m > 1 && !(length(const.vec) == m))
     stop('constant vector must be same dimension as order of multinom')
-  # if (!(gmo$ndim == 3))
-  #   stop("gmo must be for a trinomial dataset")
 
   out <- list()
-  out$bi <- sum(const.vec * gmo$p.vec[1:m])
-  out$var <- get_bi_var(gmo$var.mat, const.vec)
-  out$ci <- get_conf_int(out$bi, sqrt(out$var), gmo$num.clus - 1)
 
+  if (m == 1) {
+    out$bi <- 2*gmo$p.vec[1] - 1
+    out$var <- 4*gmo$var.mat
+  } else {
+    out$bi <- const.vec %*% gmo$p.vec[1:m]
+    out$var <- get_bi_var(gmo$var.mat, const.vec)
+  }
+
+  out$ci <- get_conf_int(out$bi, sqrt(out$var), gmo$num.clus - 1)
   class(out) <- "bi"
 
   out
